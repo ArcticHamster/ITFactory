b340fa0723511ff59894cb65a5c71623848056f3
Laur <laurentiu.cosa@gmail.com>
2023-05-15 13:19:14 +0300

    sincronizare laptop

---
+++ workshop/test.py
@@ -0,0 +1,12 @@
+def poz(list_):
+    list_poz = []
+    for number in list_:
+        if number >= 0:
+            list_poz.append(number)
+    return list_poz
+
+
+lista1 = poz([1, 5, -1, 4, -8])
+lista2 = poz([0, -7, -33, 81, 46, -11, 9])
+print(lista1)
+print(lista2)
